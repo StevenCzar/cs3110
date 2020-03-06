a7e7b22613b7bad0e108c3fc6a318a36df39cf5b
steven czarnecki <rdgNHL@gmail.com>
2020-03-06 15:58:07 -0600

    something

--- proj4/proj4.cpp
+++ proj4/proj4.cpp
@@ -9,26 +9,26 @@ void sailShip(Ship &ship);
 
 int main()
 {
-	string name;
+	std::string name;
 	double load, luxury, upper, lower;
 	int passengers, tonnage, flashpoint;
-	fstream something;
+	std::fstream something;
 	something.open("ships.txt");
 	something >> name >> load >> luxury >> upper >> lower >> passengers;
 	CruiseShip *cruiseship = new CruiseShip(name, load, luxury, upper, lower);
 	something >> name >> load >> passengers >> luxury >> tonnage >> flashpoint;
 	CargoShip *cargoship = new CargoShip(name, load, passengers, luxury);
 
-	cruiseship->fuel();
-	cruiseship->load();
-	shipSail(cruiseship);
+	cruiseship.fuel();
+	cruiseship.load();
+	shipSail(&cruiseship);
 
-	cargoship->fuel(flashpoint);
-	cargoship->load(tonnage);
-	shipSail(cargoship);
+	cargoship.fuel(flashpoint);
+	cargoship.load(tonnage);
+	shipSail(&cargoship);
 }
 
 void sailShip(Ship &ship)
 {
-	ship->sail();
+	ship.sail();
 }
