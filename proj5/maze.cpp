5491aac08f75e2a86c63d7bb7cbfab83d9e1e6c9
steven czarnecki <rdgNHL@gmail.com>
2020-03-30 14:16:36 -0500

    init

--- proj5/maze.cpp
+++ proj5/maze.cpp
@@ -73,7 +73,7 @@ void Maze::FindExit(int row, int col, bool& found)
 		{
 			//uses string.at to parse through string
 			//gets the duple for the next location to move
-			std::pair<int,int> move = getMove('N');
+			std::pair<int,int> move = getMove(xplor.at(i));
 			//move to the next space and attempt to find exit
 			FindExit(row+move.first, col+move.second, found);
 			//if you found the exit previously, keep leaving function
@@ -88,7 +88,8 @@ void Maze::FindExit(int row, int col, bool& found)
 
 //returns where to go next based on the xplor string
 std::pair<int,int> getMove(char dir)
-{	std::pair<int,int> move;
+{	
+	std::pair<int,int> move;
 	//for all directions, check if it's a valid move, then move
 	if(dir == 'N')
 	{
