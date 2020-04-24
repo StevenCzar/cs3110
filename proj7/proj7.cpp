baf530a448cd0640398bcb81ff902dd3da2803a9
steven czarnecki <rdgNHL@gmail.com>
2020-04-23 21:00:43 -0500

    idk

--- proj7/proj7.cpp
+++ proj7/proj7.cpp
@@ -2,22 +2,22 @@
 #include "priorityqueue.h"
 #include <algorithm>
 
-Card hearts[13];
-Card spades[13];
+Card heartsDeck[13];
+Card spadesDeck[13];
 PriorityQueue plswork;
 
 for(int i = 0; i<13; i++)
 {
-	hearts[i] = Card(i, hearts);
-	spades[i] = Card(i, spades);
+	heartsDeck[i] = Card(i, hearts);
+	spadesDeck[i] = Card(i, spades);
 }
 
-std::random_shuffle(std::begin(hearts), std::end(hearts));   // hearts is an array name
-std::random_shuffle(std::begin(spades), std::end(spades));   // spades is an array name
+std::random_shuffle(std::begin(heartsDeck), std::end(heartsDeck));   // hearts is an array name
+std::random_shuffle(std::begin(spadesDeck), std::end(spadesDeck));   // spades is an array name
 
 for(int i=0; i<13; i++)
 {
-	plswork.enqueue(spades[i]);
+	plswork.enqueue(spadesDeck[i]);
 	plswork.print();
 }
 
@@ -28,6 +28,6 @@ for(int i=0; i<13; i++)
 
 for(int i=0; i<13; i++)
 {
-	plswork.enqueue(hearts[i]);
+	plswork.enqueue(heartsDeck[i]);
 	plswork.print();
 }
