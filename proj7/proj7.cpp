0c157dad0617cb0d60238c6f36f7e6bcc8f82aec
steven czarnecki <rdgNHL@gmail.com>
2020-04-23 21:14:47 -0500

    idk

--- proj7/proj7.cpp
+++ proj7/proj7.cpp
@@ -2,38 +2,42 @@
 #include "priorityqueue.h"
 #include <algorithm>
 
-Card heartsDeck[13];
-Card spadesDeck[13];
-PriorityQueue<Card, lessThan<Card>> lowprio;
-PriorityQueue<Card, greaterThan<Card>> highprio;
 
-for(int i = 0; i<13; i++)
+int main()
 {
-	heartsDeck[i] = Card(i, hearts);
-	spadesDeck[i] = Card(i, spades);
-}
+	Card heartsDeck[13];
+	Card spadesDeck[13];
+	PriorityQueue<Card, lessThan<Card>> lowprio;
+	PriorityQueue<Card, greaterThan<Card>> highprio;
 
-std::random_shuffle(std::begin(heartsDeck), std::end(heartsDeck));   // hearts is an array name
-std::random_shuffle(std::begin(spadesDeck), std::end(spadesDeck));   // spades is an array name
+	for(int i = 0; i<13; i++)
+	{
+		heartsDeck[i] = Card(i, hearts);
+		spadesDeck[i] = Card(i, spades);
+	}
 
-for(int i=0; i<13; i++)
-{
-	highprio.enqueue(spadesDeck[i]);
-	highprio.print();
-}
+	std::random_shuffle(std::begin(heartsDeck), std::end(heartsDeck));   // hearts is an array name
+	std::random_shuffle(std::begin(spadesDeck), std::end(spadesDeck));   // spades is an array name
 
-for(int i=0; i<13; i++)
-{
-	highprio.dequeue(i);
-}
+	for(int i=0; i<13; i++)
+	{
+		highprio.enqueue(spadesDeck[i]);
+		highprio.print();
+	}
 
-for(int i=0; i<13; i++)
-{
-	lowprio.enqueue(heartsDeck[i]);
-	lowprio.print();
-}
+	for(int i=0; i<13; i++)
+	{
+		highprio.dequeue(i);
+	}
 
-for(int i=0; i<13; i++)
-{
-	lowprio.dequeue(i);
+	for(int i=0; i<13; i++)
+	{
+		lowprio.enqueue(heartsDeck[i]);
+		lowprio.print();
+	}
+
+	for(int i=0; i<13; i++)
+	{
+		lowprio.dequeue(i);
+	}
 }
